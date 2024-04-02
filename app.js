3ac5ef64cc9913013337d8b9bd6502d4aa0eb729
Schugu <47997052+Schugu@users.noreply.github.com>
2024-04-02 17:49:47 -0300

    Visual y comentarios.
    Mejoré el apartado visual y agregue comentarios para hacer un código mas legible.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Declaracion de variables.
 const pantallaValorAnterior = document.querySelector('.valorAnterior');
 const pantallaValorActual = document.querySelector('.valorActual');
 let botonesNumeros = document.querySelectorAll('.numero');
@@ -8,21 +9,28 @@ let botonesAccion = document.querySelectorAll('.botonAccion');
 const botonesGeneral = document.querySelectorAll('.boton');
 
 function agregarFuncionalidadBotones () {
+    // Convierto los objetos en arrays para poder iterar sobre ellos.
     botonesNumeros = Array.from(botonesNumeros);
     botonesOperadores = Array.from(botonesOperadores);
     botonesAccion = Array.from(botonesAccion);
 
+    // Itero sobre los botones
     botonesGeneral.forEach(boton => {
         boton.addEventListener('click', ()=> {
+            // Si botonesNumeros incluye el botón entonces hacer algo.
             if (botonesNumeros.includes(boton)) {
+                // Sumar el valor del boton al texto contenido en pantallaValorAnterior
                 pantallaValorAnterior.textContent += boton.value;
             } else if (botonesOperadores.includes(boton)) {
                 pantallaValorAnterior.textContent += boton.value; 
             } else if (botonesAccion.includes(boton)) {
+                // Acciones especiales para los botones especiales.
                 if (boton.value === '=') {
+                    // Declaro variables para el error.
                     const expresion = pantallaValorAnterior.textContent.trim(); 
                     const soloOperadores = /^[-+*/]+$/.test(expresion);
 
+                    // Tirar error si se trata de introducir esto.
                     if (expresion === '' || soloOperadores) {
                         pantallaValorActual.textContent = 'Sintax Error';
                     } else {
@@ -33,6 +41,7 @@ function agregarFuncionalidadBotones () {
                         }
                     }
                 } else if (boton.value === 'C') {
+                    // Esto para que el boton borrar, borre XD todo (Si existe algo en el contenedor).
                     if (pantallaValorAnterior.hasChildNodes() === true) {
                         pantallaValorAnterior.textContent = '';
                     }  
@@ -40,6 +49,7 @@ function agregarFuncionalidadBotones () {
                         pantallaValorActual.textContent = '';
                     }
                 } else if (boton.value === '←') {
+                    // Esto es para ir borrando de a un elemento.
                     if (pantallaValorAnterior.lastChild) {
                         pantallaValorAnterior.textContent = pantallaValorAnterior.textContent.slice(0, -1);
                     }
@@ -49,4 +59,5 @@ function agregarFuncionalidadBotones () {
     });
 }
 
+// Llamo a la función. 
 agregarFuncionalidadBotones();
